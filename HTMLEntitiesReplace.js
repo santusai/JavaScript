a6e32b91ccf1cc111690034fb26c5d2446f39580
santhosh janagam <sjanagamair@gmail.com>
2018-01-06 00:45:51 -0500

    Create HTMLEntitiesReplace.js

---
+++ HTMLEntitiesReplace.js
@@ -0,0 +1,7 @@
+
+function convertHTML(str) {
+  // &colon;&rpar;
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
+}
+
+convertHTML("Dolce & Gabbana");
